c93ae4283097cee03f1e84bec007bd32471d72bd
kremerdesign <kremerdesign@gmail.com>
2014-09-11 17:48:38 -0700

    static

--- snake_game/urls.py
+++ snake_game/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+from snake_game import settings
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -39,3 +41,10 @@ urlpatterns = patterns('',
 
 
 )
+
+
+if not settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
+
